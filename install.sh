312685696c16ae33ca4f8166249e6e0455f06477
dckhoa203 <khoadochi1998@gmail.com>
2022-06-24 22:17:10 +0700

    add file install & config

--- install.sh
+++ install.sh
@@ -6,12 +6,3 @@ cp .env.example env
 
 # generate key
 php artisan key:generate
-
-# Run migrate (Create tables)
-php artisan migrate
-
-# Run seed (Insert data)
-php artisan db:seed
-
-# Run
-php artisan serve
